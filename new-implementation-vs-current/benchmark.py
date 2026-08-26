64c0710327457d25b49d0cdf17bca9b2f6509563
Shaurya Sharma <65916881+shaurya-sharma-dev@users.noreply.github.com>
2026-08-26 16:53:07 -0400

    write `perc_changes` list to a text file after run completed.

--- new-implementation-vs-current/benchmark.py
+++ new-implementation-vs-current/benchmark.py
@@ -86,3 +86,7 @@ for _ in range(500):
     print(f"Percentage change:\t{perc_change:.2f}%")
 
 print(f"Average percentage change:\t{statistics.mean(perc_changes):.2f}%")
+
+import json
+with open("perc-changes.txt", "w") as f:
+    json.dump(perc_changes, f)
